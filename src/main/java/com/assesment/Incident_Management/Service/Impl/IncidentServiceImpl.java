33b557c1ecf235e401ee732c6313df26663c7078
Ayush <ayushtamrakar.work@gmail.com>
2024-07-14 13:11:28 +0530

    Added loggers

--- src/main/java/com/assesment/Incident_Management/Service/Impl/IncidentServiceImpl.java
+++ src/main/java/com/assesment/Incident_Management/Service/Impl/IncidentServiceImpl.java
@@ -10,12 +10,14 @@ import com.assesment.Incident_Management.Repository.IncidentDetailsRepo;
 import com.assesment.Incident_Management.Repository.UserDetailsRepo;
 import com.assesment.Incident_Management.Service.IncidentService;
 import com.assesment.Incident_Management.Utility.Utils;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
 import java.util.Optional;
 
+@Slf4j
 @Service
 public class IncidentServiceImpl implements IncidentService {
 
@@ -27,17 +29,22 @@ public class IncidentServiceImpl implements IncidentService {
 
     @Override
     public String createIncident(CreateIncidentRequest incidentDetails) {
+        log.info("Incident creation has started!");
         Optional<UserDetails> user = userRepo.findById(incidentDetails.getUsername());
         if(!Utils.validateOrg(incidentDetails.getOrganization())){
+            log.error("Provided organization is invalid");
             throw new InvalidValueProvidedException("Please enter valid organization");
         }
         if(!Utils.validatePriority(incidentDetails.getIncidentPriority())){
+            log.error("Provided priority is invalid");
             throw new InvalidValueProvidedException("Please enter valid priority");
         }
         if(!Utils.validateStatus(incidentDetails.getIncidentStatus())){
+            log.error("Provided status is invalid");
             throw new InvalidValueProvidedException("Please enter valid status");
         }
         if(user.isEmpty()){
+            log.error("Provided user is invalid");
             throw new InvalidValueProvidedException("Provided user is invalid");
         }
 
@@ -52,25 +59,32 @@ public class IncidentServiceImpl implements IncidentService {
                 Utils.getCurrentDateTimeAsString()
         );
         repo.save(details);
+        log.info("Incident successfully created");
         return "Incident created successfully";
     }
 
     @Override
     public String editIncident(EditIncidentRequest incidentDetails) {
+        log.info("Incident edit has started!");
         Optional<IncidentDetails> incident = repo.findById(incidentDetails.getIncidentId());
         if(incident.isEmpty()){
+            log.error("Provided organization is invalid");
             throw new DetailsNotFoundException("No incident exist for given incident id");
         }
         if(!Utils.validateOrg(incidentDetails.getOrganization())){
+            log.error("Provided organization is invalid");
             throw new InvalidValueProvidedException("Please enter valid organization");
         }
         if(!Utils.validatePriority(incidentDetails.getIncidentPriority())){
+            log.error("Provided priority is invalid");
             throw new InvalidValueProvidedException("Please enter valid priority");
         }
         if(!Utils.validateStatus(incidentDetails.getIncidentStatus())){
+            log.error("Provided status is invalid");
             throw new InvalidValueProvidedException("Please enter valid status");
         }
         if(incidentDetails.getUsername().equalsIgnoreCase(incident.get().getUserName())){
+            log.error("Provided User is invalid");
             throw new InvalidValueProvidedException("User provided in request is invalid");
         }
         IncidentDetails newIncident = new IncidentDetails(
@@ -84,15 +98,19 @@ public class IncidentServiceImpl implements IncidentService {
                 Utils.getCurrentDateTimeAsString()
         );
         repo.save(newIncident);
+        log.info("Incident updated created");
         return "Incident updated successfully";
     }
 
     @Override
     public List<IncidentDetails> getIncidentForUser(String username) {
+        log.info("Fetching Incident list for user!");
         Optional<List<IncidentDetails>> incidentList = repo.findAllIncidentDetailsByUserId(username);
         if(incidentList.isEmpty()){
+            log.error("No incident available for user");
             throw new DetailsNotFoundException("No incident for user");
         }
+        log.info("Incident list fetched successfully!");
         return incidentList.get();
     }
 }
--- src/main/java/com/assesment/Incident_Management/Service/Impl/UserLoginServiceImpl.java
+++ src/main/java/com/assesment/Incident_Management/Service/Impl/UserLoginServiceImpl.java
@@ -6,12 +6,14 @@ import com.assesment.Incident_Management.Exception.DetailsNotFoundException;
 import com.assesment.Incident_Management.Exception.InvalidValueProvidedException;
 import com.assesment.Incident_Management.Repository.UserDetailsRepo;
 import com.assesment.Incident_Management.Service.UserLoginService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import java.util.Objects;
 import java.util.Optional;
 
+@Slf4j
 @Service
 public class UserLoginServiceImpl implements UserLoginService {
 
@@ -20,19 +22,24 @@ public class UserLoginServiceImpl implements UserLoginService {
 
     @Override
     public String registerUser(UserDetails userDetails) {
+      log.info("Register user process started!");
         //Check if the user exists in database
         Optional<UserDetails> fetchUser = user.findById(userDetails.getUserName());
     if(fetchUser.isPresent()){
+        log.error("User already exists");
     throw new InvalidValueProvidedException("User Already Exists");
     }
     user.save(userDetails);
+    log.info("user registered successfully");
         return "User registered successfully!";
     }
 
     @Override
     public String updatePassword(LoginRequest request) {
+        log.info("update password process started!");
         Optional<UserDetails> fetchUser = user.findById(request.getUserName());
         if (fetchUser.isEmpty()){
+            log.error("No user exists with given username");
             throw new DetailsNotFoundException("No user exists with given username");
         }
         UserDetails userDetails = new UserDetails(
@@ -45,18 +52,23 @@ public class UserLoginServiceImpl implements UserLoginService {
                 fetchUser.get().getCountry()
                 );
         user.save(userDetails);
+        log.info("Password changed successfully");
         return "Password changed successfully";
     }
 
     @Override
     public String authenticateUser(LoginRequest request) {
+        log.info("User authentication process started!");
         Optional<UserDetails> userDetails = user.findById(request.getUserName());
         if(userDetails.isEmpty()){
+            log.error("User Doesn't Exists");
             throw new DetailsNotFoundException("User Doesn't Exists");
         }
         if(!Objects.equals(userDetails.get().getPassword(), request.getPassword())){
+            log.error("Incorrect Password provided");
             throw new InvalidValueProvidedException("Incorrect Password provided");
         }
+        log.info("User authenticate Successfully");
         return "User authenticate Successfully";
     }
 }
